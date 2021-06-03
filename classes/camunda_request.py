2309e81514f1ca293a5317d807dc46bf08aae12d
konovalov <kia.84@mail.ru>
2021-06-03 23:02:38 +0300

    feature/2: 1. add tools 3 methods for CamundaRequest 2. update http-request examples

--- classes/camunda_request.py
+++ classes/camunda_request.py
@@ -1,18 +1,86 @@
+import json
+
+import requests
+
+from settings import ACCESS_URLS
+from tools import get_token
+
 
 class CamundaRequest:
     """Запрос за получением данных"""
+    def __init__(self, env, camunda_type):
+        self.env = env
+        self.camunda_type = camunda_type
 
     def get_last_definitions(self):
-        """получаем последние версии всех развертываний"""
-        pass
+        """получаем последние версии всех развертываний
+        """
+        url = '{0}{1}{2}'.format(
+            ACCESS_URLS.get(self.env).get('camunda_url'),
+            self.camunda_type,
+            '/api/engine/engine/default/process-definition?latestVersion=true&sortBy=key&sortOrder=asc',
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        resp = requests.get(url=url, headers=headers)
+        if resp.status_code != 200:
+            print('Запрос последних версий развертываний: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
 
-    def get_definitions_by_key(self):
+    def get_definitions_by_key(self, key):
         """получаем все развертывания по ключу"""
-        pass
+        url = '{0}{1}{2}'.format(
+            ACCESS_URLS.get(self.env).get('camunda_url'),
+            self.camunda_type,
+            '/api/engine/engine/default/process-definition?key={}&sortBy=version&sortOrder=desc'.format(key),
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        resp = requests.get(url=url, headers=headers)
+        if resp.status_code != 200:
+            print('Запрос развертывания по ключу: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
 
-    def get_processes_by_definition(self):
-        """получаем список процессов по развертыванию"""
-        pass
+    def get_processes_by_definition(self, deployment_id):
+        """получаем список ЗАВЕРШЕННЫХ процессов по развертыванию"""
+        url = '{0}{1}{2}'.format(
+            ACCESS_URLS.get(self.env).get('camunda_url'),
+            self.camunda_type,
+            '/api/engine/engine/default/history/process-instance',
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        body = {
+            'processDefinitionId': deployment_id,
+            'finished': True,
+            'sorting': [
+                {
+                    'sortBy': 'businessKey',
+                    'sortOrder': 'desc'
+                },
+                {
+                    'sortBy': 'startTime',
+                    'sortOrder': 'desc'
+                }
+            ]
+        }
+        resp = requests.post(url=url, headers=headers, json=body)
+        if resp.status_code != 200:
+            print('Запрос завершенных процессов: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
 
     def get_process_variables(self):
         """получаем переменные по идентификатору процесса"""
