2f6e56731bf07ebf8750b1f48d61e347a571272b
konovalov <kia.84@mail.ru>
2021-06-11 22:57:50 +0300

    feature/2: 1. add 3 methods 2. update init

--- classes/camunda_request.py
+++ classes/camunda_request.py
@@ -11,14 +11,17 @@ class CamundaRequest:
     def __init__(self, env, camunda_type):
         self.env = env
         self.camunda_type = camunda_type
-
-    def get_last_definitions(self):
-        """получаем последние версии всех развертываний
-        """
-        url = '{0}{1}{2}'.format(
+        self.base_url = '{0}{1}{2}'.format(
             ACCESS_URLS.get(self.env).get('camunda_url'),
             self.camunda_type,
-            '/api/engine/engine/default/process-definition?latestVersion=true&sortBy=key&sortOrder=asc',
+            '/api/engine/engine/default/'
+        )
+
+    def get_last_definitions(self):
+        """получаем последние версии всех развертываний"""
+        url = '{0}{1}'.format(
+            self.base_url,
+            'process-definition?latestVersion=true&sortBy=key&sortOrder=asc',
         )
         headers = {
             'Accept': 'application/json',
@@ -33,10 +36,9 @@ class CamundaRequest:
 
     def get_definitions_by_key(self, key):
         """получаем все развертывания по ключу"""
-        url = '{0}{1}{2}'.format(
-            ACCESS_URLS.get(self.env).get('camunda_url'),
-            self.camunda_type,
-            '/api/engine/engine/default/process-definition?key={}&sortBy=version&sortOrder=desc'.format(key),
+        url = '{0}{1}'.format(
+            self.base_url,
+            'process-definition?key={0}&sortBy=version&sortOrder=desc'.format(key),
         )
         headers = {
             'Accept': 'application/json',
@@ -51,10 +53,9 @@ class CamundaRequest:
 
     def get_processes_by_definition(self, deployment_id):
         """получаем список ЗАВЕРШЕННЫХ процессов по развертыванию"""
-        url = '{0}{1}{2}'.format(
-            ACCESS_URLS.get(self.env).get('camunda_url'),
-            self.camunda_type,
-            '/api/engine/engine/default/history/process-instance',
+        url = '{0}{1}'.format(
+            self.base_url,
+            'history/process-instance',
         )
         headers = {
             'Accept': 'application/json',
@@ -82,18 +83,70 @@ class CamundaRequest:
         else:
             return json.loads(resp.content)
 
-    def get_process_variables(self):
-        """получаем переменные по идентификатору процесса"""
-        pass
+    def get_process_variables(self, process_id):
+        """получаем список переменных по идентификатору процесса"""
+        url = '{0}{1}'.format(
+            self.base_url,
+            'history/variable-instance',
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        body = {
+            "processInstanceId": process_id,
+            "sorting": [
+                {
+                  "sortBy": "variableName",
+                  "sortOrder": "asc"
+                },
+                {
+                  "sortBy": "instanceId",
+                  "sortOrder": "desc"
+                }
+              ]
+            }
+        resp = requests.post(url=url, headers=headers, json=body)
+        if resp.status_code != 200:
+            print('Запрос переменных процесса: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
 
-    def get_deserialize_variable(self):
-        """получаем конкретную переменную с сериализацией"""
-        pass
+    def get_variable(self, variable_id, deserialize=True):
+        """получаем значение переменной с/без десериализацией"""
+        url = '{0}{1}'.format(
+            self.base_url,
+            'history/variable-instance/{0}?deserializeValue={1}'.format(
+                variable_id,
+                'true' if deserialize else 'false'
+            )
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        resp = requests.get(url=url, headers=headers)
+        if resp.status_code != 200:
+            print('Запрос значения переменной: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
 
-    def get_not_deserialize_variable(self):
-        """получаем конкретную переменную без сериализации"""
-        pass
-
-    def get_incidents_by_process(self):
+    def get_incidents_by_process(self, process_id):
         """получаем историю инцидентов по processInstanceId"""
-        pass
+        url = '{0}{1}'.format(
+            self.base_url,
+            'history/incident?processInstanceId={0}'.format(process_id)
+        )
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': get_token(self.env)
+        }
+        resp = requests.get(url=url, headers=headers)
+        if resp.status_code != 200:
+            print('Запрос истории инцидентов: код ответа не равен 200!')
+            return []
+        else:
+            return json.loads(resp.content)
